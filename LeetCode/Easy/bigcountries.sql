d2e80dc412357bdb1278c3abf439749ec662e75f
Sakshi Mude <sakshimude22@gmail.com>
2026-04-23 19:50:31 +0530

    Big Countries

--- LeetCode/Easy/bigcountries.sql
+++ LeetCode/Easy/bigcountries.sql
@@ -1,4 +1,4 @@
--- problem 595 Big Countries
+-- using  SQL, write a query to find the name, population and area of the countries in the world that have an area of at least 3 million square kilometers or a population of at least 25 million people.
 SELECT name, population, area
 FROM World
 WHERE area >= 3000000 OR population >= 25000000;

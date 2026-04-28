f00a36e28df913b99107cd6639e3bfe4bef0e7d6
Sakshi Mude <sakshimude22@gmail.com>
2026-04-28 21:17:55 +0530

    Average Salary

---
+++ CodeChef/Easy/avgsalary.sql
@@ -0,0 +1,7 @@
+/*Average Salary
+Task
+Write a query to calculate the average salary 
+across all companies combined. Rename the column as avg_salary.*/
+
+select avg(salary) as avg_salary
+from Works;

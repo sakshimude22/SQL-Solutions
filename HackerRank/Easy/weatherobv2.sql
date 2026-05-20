016a331348429609ce5ad21b19b3d296dbe549c9
Sakshi Mude <sakshimude22@gmail.com>
2026-05-20 23:13:07 +0530

    Weather Observation Station 3

--- HackerRank/Easy/weatherobv2.sql
+++ HackerRank/Easy/weatherobv2.sql
@@ -1,4 +1,4 @@
--- 
+-- weather observation station 2
 SELECT 
 CAST(ROUND(SUM(LAT_N), 2) AS DECIMAL(10,2)) AS lat_sum,
 CAST(ROUND(SUM(LONG_W), 2) AS DECIMAL(10,2)) AS long_sum
---
+++ HackerRank/Easy/weatherobv3.sql
@@ -0,0 +1,2 @@
+-- weather observation station 3
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;

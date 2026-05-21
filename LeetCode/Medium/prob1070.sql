7429743605fb9acf3c308c48f92a5d9cb56d8ac6
Sakshi Mude <sakshimude22@gmail.com>
2026-05-21 08:06:17 +0530

    Product Sales Analysis III

---
+++ LeetCode/Medium/prob1070.sql
@@ -0,0 +1,16 @@
+-- 1070. Product Sales Analysis III
+select 
+s.product_id,
+s.year as first_year,
+s.quantity,
+s.price
+from Sales s
+join(
+    select 
+    product_id,
+    min(year) as first_year
+    from Sales
+    group by product_id
+) t
+on s.product_id = t.product_id
+and s.year = t.first_year;

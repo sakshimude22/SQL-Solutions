7c90a94fdfe8a35bf86ba4d19b839e1e23696073
Sakshi Mude <sakshimude22@gmail.com>
2026-05-21 08:13:33 +0530

    Product Price at a Given Date

---
+++ LeetCode/Medium/prob1164.sql
@@ -0,0 +1,21 @@
+-- 1164. Product Price at a Given Date
+select p.product_id,
+ifnull(t.new_price, 10) as price
+from (
+    select distinct product_id
+    from Products
+) p
+left join (
+    select product_id, new_price
+    from Products
+    where (product_id, change_date) in (
+        select 
+        product_id,
+        max(change_date)
+        from Products
+        where change_date <= '2019-08-16'
+        group by product_id
+    )
+
+)t
+on p.product_id = t.product_id;

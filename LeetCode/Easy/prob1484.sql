8d4d736660da81d747ae88fc6fb5475cfc9fb6ec
Sakshi Mude <sakshimude22@gmail.com>
2026-05-29 18:40:48 +0530

    Group Sold Products By The Date

---
+++ LeetCode/Easy/prob1484.sql
@@ -0,0 +1,7 @@
+-- 1484. Group Sold Products By The Date
+select sell_date,
+count(distinct product) as num_sold,
+group_concat(distinct product order by product) as products
+from Activities
+group by sell_date
+order by sell_date;

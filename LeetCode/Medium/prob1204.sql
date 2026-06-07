d0c71621b97b8ec33422133cf982c30b4c365e4f
Sakshi Mude <sakshimude22@gmail.com>
2026-06-07 11:12:42 +0530

    Last Person to Fit in the Bus

---
+++ LeetCode/Medium/prob1204.sql
@@ -0,0 +1,10 @@
+-- 1204. Last Person to Fit in the Bus
+with cte as (
+    select person_name,
+    sum(weight) over (order by turn) as total_weight
+    from Queue
+)
+select person_name from cte
+where total_weight <= 1000
+order by total_weight desc
+limit 1;

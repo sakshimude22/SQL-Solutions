4279d8eddadd58e14e448be1da73d390e13be866
Sakshi Mude <sakshimude22@gmail.com>
2026-06-11 18:36:55 +0530

    1633. Percentage of Users Attended a Contest

---
+++ LeetCode/Easy/prob1633.sql
@@ -0,0 +1,6 @@
+-- 1633. Percentage of Users Attended a Contest
+select contest_id,
+round(count(distinct user_id) * 100.0 / (select count(*) from Users), 2) as percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id asc;

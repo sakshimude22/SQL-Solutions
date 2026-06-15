6cfc80e91a4cfae528f6566989e199a44d959961
Sakshi Mude <sakshimude22@gmail.com>
2026-06-15 20:43:37 +0530

    Daily Leads and Partners

---
+++ LeetCode/Easy/prob1693.sql
@@ -0,0 +1,6 @@
+-- 1693. Daily Leads and Partners
+select date_id, make_name,
+count(distinct lead_id) as unique_leads,
+count(distinct partner_id) as unique_partners
+from dailySales
+group by date_id, make_name;

fb47e643c3e9f7f98c83256add65b185e6fdee56
Sakshi Mude <sakshimude22@gmail.com>
2026-06-24 22:52:02 +0530

    Binary Tree Nodes

---
+++ HackerRank/Medium/binary_tree_nodes.sql
@@ -0,0 +1,9 @@
+-- Binary Tree Nodes
+select N, case when P is null then 'Root'
+when N not in (
+    select distinct P from BST
+    where P is not null) then 'Leaf'
+    else 'Inner'
+    end as Type
+    from BST
+    order by N;

229889b98b023bd82888003e6f8f63f2d53233f0
Sakshi Mude <sakshimude22@gmail.com>
2026-06-27 20:22:51 +0530

    weather observation station 19

---
+++ HackerRank/Medium/weatherobv19.sql
@@ -0,0 +1,6 @@
+-- weather observation station 19
+SELECT CAST(ROUND(SQRT(
+    POWER(MAX(LAT_N) - MIN(LAT_N), 2)+
+    POWER(MAX(LONG_W) - MIN(LONG_W), 2)), 4)
+    AS DECIMAL(10,4))
+FROM STATION;

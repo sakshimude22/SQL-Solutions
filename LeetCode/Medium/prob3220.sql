0f444796d76d7b73f544d0cfd9f70c5b3b4b5e94
Sakshi Mude <sakshimude22@gmail.com>
2026-06-28 20:38:03 +0530

    Odd and Even Transactions

---
+++ LeetCode/Medium/prob3220.sql
@@ -0,0 +1,11 @@
+-- 3220. Odd and Even Transactions
+select distinct transaction_date,
+sum(case when amount % 2 = 1 then amount
+else 0
+end) as odd_sum,
+sum(case when amount % 2 = 0 then amount
+else 0
+end) as even_sum
+from transactions
+group by transaction_date
+order by transaction_date;

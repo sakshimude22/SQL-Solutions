277ce670798371f7ded2ab2bd02c4ab3e8f011ff
Sakshi Mude <sakshimude22@gmail.com>
2026-07-06 20:12:13 +0530

    Find Users with High Token Usage

---
+++ LeetCode/Easy/prob3793.sql
@@ -0,0 +1,8 @@
+-- 3793. Find Users with High Token Usage
+select user_id, 
+count(prompt) as prompt_count,
+round(avg(tokens), 2) as avg_tokens
+from prompts
+group by user_id
+having count(prompt) > 2 and max(tokens) > avg(tokens)
+order by avg_tokens desc, user_id;

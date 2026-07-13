0aa897daad25240d8293d920d66079ceead6232c
Sakshi Mude <sakshimude22@gmail.com>
2026-07-13 20:06:42 +0530

    DNA Pattern Recognition

---
+++ LeetCode/Medium/prob3475.sql
@@ -0,0 +1,8 @@
+-- 3475. DNA Pattern Recognition 
+select * ,
+case when dna_sequence like 'ATG%' then 1 else 0 end as has_start,
+case when dna_sequence like '%TAA' or dna_sequence like '%TAG' or dna_sequence like '%TGA'
+then 1 else 0 END as has_stop,
+case when dna_sequence like '%ATAT%' then 1 else 0 end as has_atat,
+case when dna_sequence like '%GGG%' then 1 else 0 end as has_ggg
+from Samples;

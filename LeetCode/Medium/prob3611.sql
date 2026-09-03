e8f3af351f3e10cd4a7ad775e3c3ac7a3362bdcf
Sakshi Mude <sakshimude22@gmail.com>
2026-09-03 13:26:57 +0530

    Find Overbooked Employees

---
+++ LeetCode/Medium/prob3611.sql
@@ -0,0 +1,14 @@
+-- 3611. Find Overbooked Employees
+# Write your MySQL query statement below
+with process_1 as (
+    select employee_id, sum(duration_hours) duration_total
+    from meetings
+    group by employee_id, WEEKOFYEAR(meeting_date), year(meeting_date)
+)
+select p.employee_id, e.employee_name, e.department, count(p.employee_id) meeting_heavy_weeks
+from process_1 p 
+inner join employees e on p.employee_id = e.employee_id
+where duration_total > 20
+group by p.employee_id, e.employee_name, e.department
+having count(p.employee_id) > 1
+order by meeting_heavy_weeks desc, employee_name;
